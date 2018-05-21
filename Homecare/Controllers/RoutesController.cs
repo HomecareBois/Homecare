b6ac27557adca5bfb9ca6ddf11303d1473a49c7a
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-05-21 14:45:52 +0200

    ladal

--- Homecare/Controllers/RoutesController.cs
+++ Homecare/Controllers/RoutesController.cs
@@ -48,43 +48,19 @@ namespace Homecare.Controllers
                         Caretaker = caretaker,
                         date = inputData.date
                     };
+                    foreach(var item in db.Routes)
+                    {
+                        if(item.date == inputData.date && item.Caretaker.caretaker_name == inputData.Caretaker.caretaker_name)
+                        {
+                            ViewBag.error = "Fejl! Du kan ikke oprette en rute for en hjemmehjælper på en dag de allerede har en rute";
+                            return View();
+                        }
+                    }
                     db.Routes.Add(route);
                     db.SaveChanges();
                 }
             }
-            return View();
+            return Redirect("Index");
         }
-        //[HttpPost]
-        //public ActionResult CreateRoute(RouteViewModel inputData)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        using (HomecareDBEntities db = new HomecareDBEntities())
-        //        {
-        //            var tidspunkt = inputData.time;
-        //            var dato = inputData.date;
-        //            var adresse = db.Patients.FirstOrDefault(pa => pa.patient_name == inputData.patientCpr).fk_address_patient;
-        //            var caretaker = db.Caretakers.FirstOrDefault(ca => ca.caretaker_name == inputData.caretaker).id_caretaker;
-
-        //            var route = new Route
-        //            {
-        //                fk_caretaker_route = caretaker,
-        //                date = dato
-        //            };
-        //            db.Routes.Add(route);
-        //            db.SaveChanges();
-        //            var routeDetails = new RouteDetail
-        //            {
-        //                arrival = tidspunkt,
-        //                fk_address_route = adresse,
-        //                fk_route_routedetails = db.Routes.FirstOrDefault(r => r.fk_caretaker_route == caretaker && r.date == dato).id_route
-        //            };
-        //            db.RouteDetails.Add(routeDetails);
-        //            db.SaveChanges();
-        //        }
-        //        ModelState.Clear();
-
-        //    }
-        //    return View();
     }
 }
